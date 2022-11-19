702a71d36db57f15db1598f0d16f1eb67bc2e792
Reski Mulud Muchamad <reski.mulud@gmail.com>
2022-11-19 16:20:44 +0700

    HelloJetpackCompose: basic col and row

--- HelloJetpackCompose/app/src/main/java/com/mankart/hellojetpackcompose/MainActivity.kt
+++ HelloJetpackCompose/app/src/main/java/com/mankart/hellojetpackcompose/MainActivity.kt
@@ -3,14 +3,20 @@ package com.mankart.hellojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mankart.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +38,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Dicoding")
+        } 
+    }
 }
 
 @Preview(
