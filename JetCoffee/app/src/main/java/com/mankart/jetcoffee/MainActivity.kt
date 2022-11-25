df3feee8a7688eef4873f8d2c412adc88cd1607a
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-11-25 20:01:28 +0700

    JetCoffee: create menu row

--- JetCoffee/app/src/main/java/com/mankart/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/mankart/jetcoffee/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -15,8 +17,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mankart.jetcoffee.model.Menu
+import com.mankart.jetcoffee.model.dummyBestSellerMenu
 import com.mankart.jetcoffee.model.dummyCategory
+import com.mankart.jetcoffee.model.dummyMenu
 import com.mankart.jetcoffee.ui.components.CategoryItem
+import com.mankart.jetcoffee.ui.components.MenuItem
 import com.mankart.jetcoffee.ui.components.SearchBar
 import com.mankart.jetcoffee.ui.components.SectionText
 import com.mankart.jetcoffee.ui.theme.JetCoffeeTheme
@@ -34,10 +40,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(title = stringResource(id = R.string.section_category))
         CategoryRow()
+        SectionText(title = stringResource(id = R.string.section_favorite_menu))
+        MenuRow(listMenu = dummyMenu)
+        SectionText(title = stringResource(id = R.string.section_best_seller_menu))
+        MenuRow(listMenu = dummyBestSellerMenu)
     }
 }
 
@@ -63,6 +73,22 @@ fun CategoryRow(modifier: Modifier = Modifier) {
     ) {
         items(dummyCategory, key = { it.textCategory }) { category ->
             CategoryItem(category = category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
         }
     }
 }
