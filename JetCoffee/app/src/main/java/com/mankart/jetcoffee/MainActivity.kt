b7956cfbd4cbf09c10f8af8acbba2b2220d0d931
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-11-26 21:02:04 +0700

    JetCoffee: create bottom bar composable function

--- JetCoffee/app/src/main/java/com/mankart/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/mankart/jetcoffee/MainActivity.kt
@@ -9,18 +9,24 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mankart.jetcoffee.model.Menu
-import com.mankart.jetcoffee.model.dummyBestSellerMenu
-import com.mankart.jetcoffee.model.dummyCategory
-import com.mankart.jetcoffee.model.dummyMenu
+import com.mankart.jetcoffee.model.*
 import com.mankart.jetcoffee.ui.components.CategoryItem
 import com.mankart.jetcoffee.ui.components.MenuItem
 import com.mankart.jetcoffee.ui.components.SearchBar
@@ -54,6 +60,40 @@ fun JetCoffeeApp() {
             title = stringResource(id = R.string.section_best_seller_menu),
             content = { MenuRow(listMenu = dummyBestSellerMenu) }
         )
+    }
+}
+
+@Composable
+fun BottomBar(modifier: Modifier = Modifier) {
+    BottomNavigation(modifier = modifier) {
+        val navigationItems: List<BottomBarItem> = listOf(
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            )
+        )
+
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(imageVector = it.icon, contentDescription = it.title)
+                },
+                label = {
+                    Text(text = it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = Color.LightGray,
+                onClick = {}
+            )
+        }
     }
 }
 
---
+++ JetCoffee/app/src/main/java/com/mankart/jetcoffee/model/BottomBarItem.kt
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2022
+ * Project  : Jet Coffee
+ * Created by Reski Mulud Muchamad on 26-11-2022
+ * GitHub   : https://github.com/reskimulud
+ * LinkedIn : https://linkedin.com/in/reskimulud
+ */
+
+package com.mankart.jetcoffee.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(
+    val title: String,
+    val icon: ImageVector
+)
